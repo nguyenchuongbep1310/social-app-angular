293ab484c5d134c72dd2163fa596cd8a51299fa8
Trung Phan Van <trung.phan@orientsoftware.com>
2022-06-16 13:05:54 +0700

    byT update register api, create unit test

---
+++ DatingApp.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DatingApp.Core.Entities;
+
+namespace DatingApp.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        IEnumerable<AppUser> GetAll();
+        AppUser GetById(int userID);
+        void Insert(AppUser user);
+        void Update(AppUser user);
+        void Delete(int userID);
+        void Save();
+        Task<bool> CheckEmailExist(string userEmail);
+        Task<bool> CheckUsernameExist(string username);
+    }
+}
