d64dd70c86571fde9e257e0bf07bb225470c7531
Trung Phan Van <trung.phan@orientsoftware.com>
2022-06-21 17:54:03 +0700

    byT update upload file

---
+++ DatingApp.Application/DTO/ProfileDto.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.Application.DTO
+{
+    public class ProfileDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Phone { get; set; }
+
+        public IFormFile Avatar { get; set; }
+        public IFormFile CoverPhoto { get; set; }
+    }
+}
