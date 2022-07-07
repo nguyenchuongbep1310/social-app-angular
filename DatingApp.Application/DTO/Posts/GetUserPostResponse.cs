6f8e7cf4398278900912fb24636867a40589c2dc
Trung Phan Van <trung.phan@orientsoftware.com>
2022-07-07 10:11:28 +0700

    byT update post api

---
+++ DatingApp.Application/DTO/Posts/GetUserPostResponse.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.Application.DTO.Posts
+{
+    public class GetUserPostResponse
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Text { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
